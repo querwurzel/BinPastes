43d3062b1ed47f74999b9db19d0ed4f1a1aaa718
Stefan Wilke <stefan.wilke1@axelspringer.com>
2022-12-16 15:44:39 +0100

    fix parameter binding

--- backend/src/main/java/it/wylke/binpastes/paste/domain/MySqlFullTextSupportImpl.java
+++ backend/src/main/java/it/wylke/binpastes/paste/domain/MySqlFullTextSupportImpl.java
@@ -1,5 +1,6 @@
 package it.wylke.binpastes.paste.domain;
 
+import io.r2dbc.spi.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -40,12 +41,7 @@ class MySqlFullTextSupportImpl implements FullTextSearchSupport {
 
         var connectionFactory = entityTemplate.getDatabaseClient().getConnectionFactory();
 
-        var query = String.format("""
-                    SELECT * FROM %s
-                    WHERE %s = ?exposure
-                    AND %s IS NULL
-                    AND (%s IS NULL OR %s > ?expiryAfter)
-                    AND MATCH(%s, %s) AGAINST(?text IN BOOLEAN MODE)""".strip(),
+        var query = String.format("SELECT * FROM %s WHERE %s = ?exposure AND %s IS NULL AND (%s IS NULL OR %s > ?expiryAfter) AND MATCH(%s, %s) AGAINST(?text IN BOOLEAN MODE)".strip(),
                 PasteSchema.TABLE_NAME,
                 PasteSchema.EXPOSURE,
                 PasteSchema.DATE_DELETED,
@@ -56,37 +52,37 @@ class MySqlFullTextSupportImpl implements FullTextSearchSupport {
         );
 
         return Mono.from(connectionFactory.create())
-            .flatMap(mySqlConnection -> Mono.from(mySqlConnection
-                        .createStatement(query)
-                        .bind("exposure", PasteExposure.PUBLIC.name())
-                        .bind("expiryAfter", LocalDateTime.now())
-                        .bind("text", text + '*')
-                        .execute()
-            ))
-            .flatMapMany(mySqlResult -> Flux.from(mySqlResult.map((row, rowMetadata) -> {
-                var paste = new Paste();
-                paste.setId(row.get(PasteSchema.ID, String.class));
-                paste.setVersion(row.get(PasteSchema.VERSION, Long.class));
+                .flatMap(mySqlConnection -> Mono.from(mySqlConnection
+                            .createStatement(query)
+                            .bind("exposure", PasteExposure.PUBLIC.name())
+                            .bind("expiryAfter", LocalDateTime.now())
+                            .bind("text", text + '*')
+                            .execute()
+                ))
+                .flatMapMany(mySqlResult -> Flux.from(mySqlResult.map((row, rowMetadata) -> {
+                    var paste = new Paste();
+                    paste.setId(row.get(PasteSchema.ID, String.class));
+                    paste.setVersion(row.get(PasteSchema.VERSION, Long.class));
 
-                paste.setTitle(row.get(PasteSchema.TITLE) == null
-                        ? null
-                        : row.get(PasteSchema.TITLE, String.class));
-                paste.setContent(row.get(PasteSchema.CONTENT, String.class));
-                paste.setIsEncrypted(Boolean.parseBoolean(row.get(PasteSchema.IS_ENCRYPTED, String.class)));
-                paste.setExposure(PasteExposure.valueOf(row.get(PasteSchema.EXPOSURE, String.class)));
+                    paste.setTitle(row.get(PasteSchema.TITLE) == null
+                            ? null
+                            : row.get(PasteSchema.TITLE, String.class));
+                    paste.setContent(row.get(PasteSchema.CONTENT, String.class));
+                    paste.setIsEncrypted(Boolean.parseBoolean(row.get(PasteSchema.IS_ENCRYPTED).toString()));
+                    paste.setExposure(row.get(PasteSchema.EXPOSURE, PasteExposure.class));
 
-                paste.setDateCreated(LocalDateTime.parse(row.get(PasteSchema.DATE_CREATED, CharSequence.class)));
-                paste.setDateOfExpiry(row.get(PasteSchema.DATE_OF_EXPIRY) == null
-                        ? null
-                        : LocalDateTime.parse(row.get(PasteSchema.DATE_OF_EXPIRY, CharSequence.class)));
-                paste.setDateDeleted(row.get(PasteSchema.DATE_DELETED) == null
-                        ? null
-                        : LocalDateTime.parse(row.get(PasteSchema.DATE_DELETED, CharSequence.class)));
+                    paste.setDateCreated(LocalDateTime.parse(row.get(PasteSchema.DATE_CREATED).toString()));
+                    paste.setDateOfExpiry(row.get(PasteSchema.DATE_OF_EXPIRY) == null
+                            ? null
+                            : LocalDateTime.parse(row.get(PasteSchema.DATE_OF_EXPIRY).toString()));
+                    paste.setDateDeleted(row.get(PasteSchema.DATE_DELETED) == null
+                            ? null
+                            : LocalDateTime.parse(row.get(PasteSchema.DATE_DELETED).toString()));
 
-                paste.setRemoteAddress(row.get(PasteSchema.REMOTE_ADDRESS) == null
-                        ? null
-                        : row.get(PasteSchema.REMOTE_ADDRESS, String.class));
-                return paste;
-            })));
+                    paste.setRemoteAddress(row.get(PasteSchema.REMOTE_ADDRESS) == null
+                            ? null
+                            : row.get(PasteSchema.REMOTE_ADDRESS, String.class));
+                    return paste;
+                })));
     }
 }
