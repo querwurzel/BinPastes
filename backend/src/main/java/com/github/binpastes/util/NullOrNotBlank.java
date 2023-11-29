a2bb0f2f53e4fb8a1a82a569dc5c8f444f612efd
querwurzel <>
2023-11-29 10:51:34 +0100

    fix title validation on creation, add test

---
+++ backend/src/main/java/com/github/binpastes/util/NullOrNotBlank.java
@@ -0,0 +1,29 @@
+package com.github.binpastes.util;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target( {ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = NullOrNotBlank.NullOrNotBlankValidator.class)
+public @interface NullOrNotBlank {
+    String message() default "{jakarta.validation.constraints.NotBlank.message}";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default {};
+
+    class NullOrNotBlankValidator implements ConstraintValidator<NullOrNotBlank, String> {
+
+        public void initialize(NullOrNotBlank parameters) {
+            // nothing to do here
+        }
+
+        public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+            return value == null || value.trim().length() > 0;
+        }
+    }
+}
