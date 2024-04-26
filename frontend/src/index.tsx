20da728a798475c58cc2cbcefb04f8570c27d8de
querwurzel <>
2024-04-26 22:46:49 +0200

    Bump the frontend group in /frontend with 5 updates (#103)
    Bumps the frontend group in /frontend with 5 updates:
    
    | Package | From | To |
    | --- | --- | --- |
    | [@solidjs/router](https://github.com/solidjs/solid-router) | `0.9.1` | `0.10.4` |
    | [pnpm](https://github.com/pnpm/pnpm) | `8.11.0` | `8.12.1` |
    | [typescript](https://github.com/Microsoft/TypeScript) | `5.3.2` | `5.3.3` |
    | [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) | `5.0.4` | `5.0.10` |
    | [vite-plugin-solid](https://github.com/solidjs/vite-plugin-solid) | `2.7.2` | `2.8.0` |
    
    Updates `@solidjs/router` from 0.9.1 to 0.10.4
    - [Commits](https://github.com/solidjs/solid-router/commits)
    
    Updates `pnpm` from 8.11.0 to 8.12.1
    - [Release notes](https://github.com/pnpm/pnpm/releases)
    - [Commits](https://github.com/pnpm/pnpm/compare/v8.11.0...v8.12.1)
    
    Updates `typescript` from 5.3.2 to 5.3.3
    - [Release notes](https://github.com/Microsoft/TypeScript/releases)
    - [Commits](https://github.com/Microsoft/TypeScript/compare/v5.3.2...v5.3.3)
    
    Updates `vite` from 5.0.4 to 5.0.10
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v5.0.10/packages/vite)
    
    Updates `vite-plugin-solid` from 2.7.2 to 2.8.0
    - [Release notes](https://github.com/solidjs/vite-plugin-solid/releases)
    - [Changelog](https://github.com/solidjs/vite-plugin-solid/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/solidjs/vite-plugin-solid/commits)
    
    ---
    updated-dependencies:
    - dependency-name: "@solidjs/router"
      dependency-type: direct:production
      update-type: version-update:semver-minor
      dependency-group: frontend
    - dependency-name: pnpm
      dependency-type: direct:development
      update-type: version-update:semver-minor
      dependency-group: frontend
    - dependency-name: typescript
      dependency-type: direct:development
      update-type: version-update:semver-patch
      dependency-group: frontend
    - dependency-name: vite
      dependency-type: direct:development
      update-type: version-update:semver-patch
      dependency-group: frontend
    - dependency-name: vite-plugin-solid
      dependency-type: direct:development
      update-type: version-update:semver-minor
      dependency-group: frontend
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,20 @@
 /* @refresh reload */
-import {Router} from '@solidjs/router';
-import {JSX} from 'solid-js';
+import {Router, Route} from '@solidjs/router';
+import {JSX, lazy} from 'solid-js';
 import {render} from 'solid-js/web';
 import App from './App';
+import Create from './pages/create';
+import Search from './pages/search';
 import './index.css';
 
+const Read = lazy(() => import('./pages/read'));
+const NotFound = lazy(() => import('./pages/404'));
+
 render((): JSX.Element => (
-  <Router>
-    <App />
+  <Router root={App}>
+    <Route path="/" component={Create}  />
+    <Route path="/paste/:id" component={Read} />
+    <Route path="/paste/search" component={Search} />
+    <Route path="*" component={NotFound} />
   </Router>
 ), document.getElementById('root'));
