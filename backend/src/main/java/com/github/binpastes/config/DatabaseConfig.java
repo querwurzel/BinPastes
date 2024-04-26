954ed40525f0294140703ecea22ed05b6848110f
querwurzel <>
2024-04-26 22:46:50 +0200

    fix(r2dbc): config now links to SpringBootApplication

--- backend/src/main/java/com/github/binpastes/config/DatabaseConfig.java
+++ backend/src/main/java/com/github/binpastes/config/DatabaseConfig.java
@@ -1,10 +1,11 @@
 package com.github.binpastes.config;
 
+import com.github.binpastes.BinPastes;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
-@EnableR2dbcRepositories
+@EnableR2dbcRepositories(basePackageClasses = BinPastes.class)
 @EnableR2dbcAuditing
 public class DatabaseConfig {}
