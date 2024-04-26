4d3db0eca1bbdc6ff8604364e40dcc4227d13c80
querwurzel <>
2024-04-26 22:46:49 +0200

    add more tests for creation

--- backend/src/test/java/com/github/binpastes/paste/api/OneTimePastesIT.java
+++ backend/src/test/java/com/github/binpastes/paste/api/OneTimePastesIT.java
@@ -10,9 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+import static java.time.LocalDateTime.now;
+import static java.time.LocalDateTime.parse;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -84,6 +88,51 @@ class OneTimePastesIT {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody().jsonPath("pastes", emptyList());
+    }
+
+    @Test
+    @DisplayName("POST / - one-time paste is created using all options")
+    void createOneTimePaste() {
+        webClient.post()
+                .uri("/api/v1/paste")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just("""
+                        {
+                            "title": "someTitle",
+                            "content": "someContent",
+                            "exposure": "ONCE",
+                            "isEncrypted": true,
+                            "expiry": "THREE_MONTHS"
+                        }
+                        """), String.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().cacheControl(CacheControl.empty())
+                .expectBody()
+                .jsonPath("$.id").<String>value(id ->
+                        assertThat(id).matches("^[a-zA-Z0-9]{40}$")
+                )
+                .jsonPath("$.dateCreated").<String>value(dateCreated ->
+                        assertThat(parse(dateCreated)).isBefore(now())
+                )
+                .jsonPath("$.dateOfExpiry").<String>value(dateOfExpiry ->
+                        assertThat(parse(dateOfExpiry))
+                                .isBefore(now().plusMonths(3))
+                                .isAfter(now().plusMonths(3).minusDays(1))
+                )
+                .json("""
+                            {
+                              "title": "someTitle",
+                              "content": "someContent",
+                              "sizeInBytes": 11,
+                              "isPublic": false,
+                              "isErasable": true,
+                              "isEncrypted": true,
+                              "isOneTime": true,
+                              "lastViewed": null,
+                              "views": 0
+                            }
+                """, false);
     }
 
     @Test
