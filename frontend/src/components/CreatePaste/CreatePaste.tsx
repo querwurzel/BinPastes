992ad34cf87c6629d838fe9cc6f78dd5b13e3548
querwurzel <>
2024-04-26 22:46:48 +0200

    remove submit shortcut

--- frontend/src/components/CreatePaste/CreatePaste.tsx
+++ frontend/src/components/CreatePaste/CreatePaste.tsx
@@ -1,8 +1,8 @@
-import {Component, createSignal, JSX, onMount, Show} from 'solid-js';
-import {createStore} from 'solid-js/store';
-import {PasteCreateCmd} from '../../api/model/PasteCreateCmd';
-import {encrypt} from '../../crypto/CryptoUtil';
-import styles from './createPaste.module.css';
+import {Component, createSignal, JSX, Show} from "solid-js";
+import {createStore} from "solid-js/store";
+import {PasteCreateCmd} from "../../api/model/PasteCreateCmd";
+import {encrypt} from "../../crypto/CryptoUtil";
+import styles from "./createPaste.module.css";
 
 export interface CloneModel {
   title?: string
@@ -14,7 +14,7 @@ interface CreatePasteProps {
   initialValues?: CloneModel
 }
 
-interface FormModel extends Omit<PasteCreateCmd, 'isEncrypted'> {
+interface FormModel extends Omit<PasteCreateCmd, "isEncrypted"> {
   password: string
 }
 
@@ -31,20 +31,6 @@ const CreatePaste: Component<CreatePasteProps> = ({onCreatePaste, initialValues}
   const [lastPaste, setLastPaste] = createSignal<string>(null);
 
   let creationForm: HTMLFormElement
-
-  onMount(() => {
-    creationForm.onkeydown = submitCreateFormHotKey
-  })
-
-  const submitCreateFormHotKey = (e: KeyboardEvent) => {
-    if (e.altKey || e.shiftKey || e.metaKey) {
-      return;
-    }
-
-    if (e.ctrlKey && e.code === 'Enter') {
-      creationForm.requestSubmit();
-    }
-  }
 
   const updateFormField = (fieldName: keyof FormModel) => (event: Event) => {
     const inputElement = event.currentTarget as HTMLInputElement;
@@ -96,10 +82,9 @@ const CreatePaste: Component<CreatePasteProps> = ({onCreatePaste, initialValues}
       <fieldset>
         <div>
           <label for="expiry">Expires in: </label>
-          <select id="expiry" name="expiry" onChange={updateFormField('expiry')}>
+          <select id="expiry" name="expiry" onChange={updateFormField("expiry")}>
             <option value="ONE_HOUR">1 Hour</option>
-            {/* @ts-ignore selected={true} malfunctioning, is not rendered */}
-            <option value="ONE_DAY" /* selected={true} */ selected="selected">1 Day</option>
+            <option value="ONE_DAY" selected={true}>1 Day</option>
             <option value="ONE_WEEK">1 Week</option>
             <option value="ONE_MONTH">1 Month</option>
             <option value="THREE_MONTHS">3 Months</option>
@@ -111,39 +96,39 @@ const CreatePaste: Component<CreatePasteProps> = ({onCreatePaste, initialValues}
         <div>
           <label>Visibility: </label>
           <label for="public">
-            {/* @ts-ignore checked={true} malfunctioning, is not rendered */}
-            <input type="radio" id="public" name="exposure" value="PUBLIC" /* checked={true} */ checked="checked" onInput={updateFormField('exposure')}/>
+            <input type="radio" id="public" name="exposure" value="PUBLIC" checked={true}
+                   onInput={updateFormField("exposure")}/>
             Public
           </label>
           <label for="unlisted">
-            <input type="radio" id="unlisted" name="exposure" value="UNLISTED" onInput={updateFormField('exposure')}/>
+            <input type="radio" id="unlisted" name="exposure" value="UNLISTED" onInput={updateFormField("exposure")}/>
             Unlisted
           </label>
           <label for="once">
-            <input type="radio" id="once" name="exposure" value="ONCE" onInput={updateFormField('exposure')}/>
+            <input type="radio" id="once" name="exposure" value="ONCE" onInput={updateFormField("exposure")}/>
             Once (One-Time)
           </label>
         </div>
         <hr/>
         <div>
-          <label htmlFor="title">Title (optional): </label>
+          <label for="title">Title (optional): </label>
           <input type="text"
                  id="title"
                  name="title"
-                 placeholder={'Title'}
+                 placeholder="Title"
                  maxLength={255}
                  value={form.title}
-                 onInput={updateFormField('title')}/>
+                 onInput={updateFormField("title")}/>
         </div>
         <hr/>
         <div>
-          <label htmlFor="key">Password (optional): </label>
+          <label for="key">Password (optional): </label>
           <input id="key"
                  name="key"
                  type="password"
-                 placeholder={'Password'}
-                 autoComplete="one-time-code"
-                 onInput={updateFormField('password')}/>
+                 placeholder="Password"
+                 autocomplete="one-time-code"
+                 onInput={updateFormField("password")}/>
         </div>
         <hr/>
         <div>
@@ -153,8 +138,8 @@ const CreatePaste: Component<CreatePasteProps> = ({onCreatePaste, initialValues}
                     autofocus={true}
                     rows="20"
                     cols="75"
-                    placeholder={'Paste here'}
-                    onInput={updateFormField('content')}>{form.content}</textarea>
+                    placeholder="Paste here"
+                    onInput={updateFormField("content")}>{form.content}</textarea>
         </div>
       </fieldset>
 
@@ -162,8 +147,8 @@ const CreatePaste: Component<CreatePasteProps> = ({onCreatePaste, initialValues}
         <Show when={lastPaste()}>
           <p class={styles.lastPaste}><span>{lastPaste()}</span> ⎘</p>
         </Show>
-        <input type="submit" value="Paste Ctrl+⏎" />
-        <input type="reset" value="Reset" />
+        <input type="submit" value="Paste"/>
+        <input type="reset" value="Reset"/>
       </fieldset>
 
     </form>
