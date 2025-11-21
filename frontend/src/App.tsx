a2660a9fe9100a59d4722df08ba7a45639e10f72
querwurzel <>
2025-11-22 00:17:45 +0100

    hide aside when on mobile read page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import {JSX, Component, ParentProps} from 'solid-js';
+import { useLocation } from "@solidjs/router";
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import RecentPastes from './components/RecentPastes/RecentPastes';
 import styles from './App.module.css';
 
 const App: Component = (props: ParentProps): JSX.Element => {
+
+  const location = useLocation();
+
   return (
     <>
       <header class={styles.header}>
@@ -19,7 +23,7 @@ const App: Component = (props: ParentProps): JSX.Element => {
           </div>
         </main>
 
-        <aside class={styles.right}>
+        <aside classList={{ [styles.right] : true, [styles.rightMobile]: location.pathname.startsWith("/paste/") }}>
           <RecentPastes />
         </aside>
 
